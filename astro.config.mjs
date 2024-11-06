573967a3a147ac09af6f14b7800c2389113ab627
Travis <unkletas@gmail.com>
2024-11-06 16:51:59 -0600

    Rebuilt ContactForm.astro to work with Web3Forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-// astro.config.mjs
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercel(),
 });
